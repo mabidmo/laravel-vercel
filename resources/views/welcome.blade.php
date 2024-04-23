6795dfa284d34ee9b504ba2c616078440875af34
MSI\mabid <mabidmurtadho@gmail.com>
2024-04-23 14:10:23 +0700

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Deploy Vercel @mabimurtadho</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
